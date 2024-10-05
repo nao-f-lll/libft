e58bb2f9e93321c264e06764e6fa44cbdcb9c32d
Naoufal Lyamani <nlyamani@c4r4s4.42urduliz.com>
2024-10-05 19:29:31 +0200

    ft_split is now adapted to the norm;; starting to implement the bonus part of the project

---
+++ ft_lstnew_bonus.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew_bonus.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nlyamani <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/05 18:19:09 by nlyamani          #+#    #+#             */
+/*   Updated: 2024/10/05 19:01:04 by nlyamani         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*list;
+
+	list = (t_list *)malloc(sizeof(t_list));
+	if (!list)
+		return (NULL);
+	list->content = content;
+	list->next = NULL;
+	return (list);
+}
--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: nlyamani <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 17:49:27 by nlyamani          #+#    #+#             */
-/*   Updated: 2024/10/05 15:27:37 by nlyamani         ###   ########.fr       */
+/*   Updated: 2024/10/05 18:09:35 by nlyamani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,19 @@
 static int	ft_word_count(const char *s, char c)
 {
 	int	w_c;
-	int	i;
+	int	w_in;
 
 	w_c = 0;
-	i = 0;
+	w_in = 0;
 	while (*s)
 	{
-		if (*s != c && i == 0)
+		if (*s != c && w_in == 0)
 		{
-			i = 1;
+			w_in = 1;
 			w_c++;
 		}
 		else if (*s == c)
-			i = 0;
+			w_in = 0;
 		s++;
 	}
 	return (w_c);
@@ -57,33 +57,25 @@ static int	find_word_end(const char *s, char c)
 char	**ft_split(const char *s, char c)
 {
 	char	**words;
-	int		ws_c;
-	int		w_ed;
-	int		s_i;
 	int		words_i;
 
-	s_i = 0;
 	words_i = 0;
 	if (!s)
 		return (NULL);
-	ws_c = ft_word_count(s, c);
-	if (ws_c < 0)
-		return (NULL);
-	words = malloc((ws_c + 1) * sizeof(char *));
+	words = malloc((ft_word_count(s, c) + 1) * sizeof(char *));
 	if (!words)
 		return (NULL);
-	while (s[s_i])
+	while (*s)
 	{
-		while (s[s_i] == c)
-			s_i++;
-		if (s[s_i])
+		while (*s == c)
+			s++;
+		if (*s)
 		{
-			w_ed = find_word_end(s + s_i, c);
-			words[words_i] = ft_substr(s + s_i, 0, w_ed);
+			words[words_i] = ft_substr(s, 0, find_word_end(s, c));
 			if (!words[words_i])
 				return (ft_free(words, words_i), NULL);
 			words_i++;
-			s_i += w_ed;
+			s += find_word_end(s, c);
 		}
 	}
 	words[words_i] = NULL;
